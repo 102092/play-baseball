58d5da5cf55005b820d0e50f98a91f02851de140
KIM DONG HWAN <quaint3@gmail.com>
2019-12-03 17:45:08 +0900

    README 설명 추가, 필요없는 주석 코드 삭제
    step3 코드 동작에 해당하는 설명을 추가.
    util 클래스 주석 부분을 삭제.

--- src/com/baseball02/util/DataInputUtil.java
+++ src/com/baseball02/util/DataInputUtil.java
@@ -5,13 +5,10 @@ import java.util.Scanner;
 public class DataInputUtil {
 
 	public static String getInput() {
-//		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
-//		String input = bf.readLine();
-//		return input;
-		
+
 		Scanner sc = new Scanner(System.in);
 		String input = sc.nextLine();
 		return input;
 	}
-	
+
 }
